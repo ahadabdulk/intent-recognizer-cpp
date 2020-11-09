111f5631d2de3e43675619b562d9a77c8f251e0d
Abdul Ahad <62215885+ahadabdulk@users.noreply.github.com>
2020-11-09 23:11:20 +0100

    Updating the logic to fix failed unit test

--- src/IntentFactory.cpp
+++ src/IntentFactory.cpp
@@ -5,20 +5,19 @@
 #include "IntentCalender.h"
 #include "IntentFact.h"
 
-
 std::shared_ptr<Intent> IntentFactory::CreateIntent(const std::string& str)
 {
-    std::regex regexWeatherIntent("([\\w\\s]+[Ww]eather\\slike\\s[\\w]+)", std::regex_constants::icase );
-    std::regex regexWeatherCityIntent("([\\w\\s]+[Ww]eather)\\slike\\sin\\s[\\w\\s]+", std::regex_constants::icase );
-    std::regex regexCalenderIntent("([\\w\\s]+[0-9]?[0-9]{1}:[0-5]{1}[0-9]{1}\\s(AM|PM)[\\w\\s]+)", std::regex_constants::icase );
+    std::regex regexWeatherIntent("([\\w\\s]+[Ww]eather\\slike\\s[\\w]+\?)", std::regex_constants::icase );
+    std::regex regexWeatherCityIntent("([\\w\\s]+[Ww]eather)\\slike\\sin\\s[\\w\\s]+\?)", std::regex_constants::icase );
+    std::regex regexCalenderIntent("([\\w\\s]+[0-9]?[0-9]{1}:[0-5]{1}[0-9]{1}\\s(AM|PM)[\\w\\s]+\?)", std::regex_constants::icase );
 
-    if( ConfirmIntent(str, regexWeatherIntent) )
-    {
-        return std::make_shared<IntentWeather>();
-    }
-    else if( ConfirmIntent(str, regexWeatherCityIntent ) )
+    if( ConfirmIntent(str, regexWeatherCityIntent) )
     {
         return std::make_shared<IntentWeatherCity>();
+    }
+    else if( ConfirmIntent(str, regexWeatherIntent ) )
+    {
+        return std::make_shared<IntentWeather>();
     }
     else if( ConfirmIntent(str, regexCalenderIntent) )
     {
