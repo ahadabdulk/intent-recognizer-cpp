be293e0ddddb7ed330aeb44c96d96e757b28def7
Abdul Ahad <62215885+ahadabdulk@users.noreply.github.com>
2020-11-09 22:17:46 +0100

    Updating factory to create different concrete intents

--- src/IntentFactory.cpp
+++ src/IntentFactory.cpp
@@ -1,5 +1,9 @@
-#include "../include/Intent.h"
-#include "../include/IntentFactory.h"
+#include "Intent.h"
+#include "IntentFactory.h"
+#include "IntentWeather.h"
+#include "IntentWeatherCity.h"
+#include "IntentCalender.h"
+#include "IntentFact.h"
 
 
 std::shared_ptr<Intent> IntentFactory::CreateIntent(const std::string& str)
@@ -8,10 +12,39 @@ std::shared_ptr<Intent> IntentFactory::CreateIntent(const std::string& str)
     std::regex regexWeatherCityIntent("([\\w\\s]+[Ww]eather)\\slike\\sin\\s[\\w\\s]+", std::regex_constants::icase );
     std::regex regexCalenderIntent("([\\w\\s]+[0-9]?[0-9]{1}:[0-5]{1}[0-9]{1}\\s(AM|PM)[\\w\\s]+)", std::regex_constants::icase );
 
-    return std::make_shared<Intent>(nullptr);
+    if( ConfirmIntent(str, regexWeatherIntent) )
+    {
+        return std::make_shared<IntentWeather>();
+    }
+    else if( ConfirmIntent(str, regexWeatherCityIntent ) )
+    {
+        return std::make_shared<IntentWeatherCity>();
+    }
+    else if( ConfirmIntent(str, regexCalenderIntent) )
+    {
+        return std::make_shared<IntentCalender>();
+    }
+    else
+    {
+        return std::make_shared<IntentFact>();
+    }
+
 }
 
 bool IntentFactory::ConfirmIntent(const std::string& str, const std::regex& reg ) const
 {
-    return false;
+    bool hasMatched = false;
+
+    std::sregex_iterator currentMatch(str.begin(), str.end(), reg);
+    std::sregex_iterator lastMatch;
+    std::smatch match;
+
+    while( currentMatch != lastMatch ){
+        match = *currentMatch;
+        currentMatch++;
+    } 
+    
+    hasMatched = match.ready();
+
+    return hasMatched;
 }
